bad538f73083b998b8a07d10f5cbe59528dc1c2e
Omar Hyjek <ohyjek@gmail.com>
2019-06-14 15:05:28 -0400

    add grommet

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Grommet } from "grommet";
+
+//theme
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px"
+    }
+  }
+};
 
 export function App() {
-  return <h1>All The Things!</h1>;
+  return (
+    <Grommet theme={theme}>
+      <h1>All The Things!</h1>
+    </Grommet>
+  );
 }
