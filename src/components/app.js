9a799028332bddaf6e0d0d973136fbcb74327a8c
Omar Hyjek <ohyjek@gmail.com>
2019-06-14 15:19:53 -0400

    add version to header

--- src/components/app.js
+++ src/components/app.js
@@ -15,7 +15,7 @@ const theme = {
 export function App() {
   return (
     <Grommet theme={theme}>
-      <h1>All The Things!</h1>
+      <h1>All The Things! v({process.env.npm_package_version})</h1>
     </Grommet>
   );
 }
