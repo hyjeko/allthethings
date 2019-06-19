fd1db165fabcae044bb2d2afb6437de1692e6486
Omar Hyjek <ohyjek@gmail.com>
2019-06-19 16:04:31 -0400

    implement add functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,14 @@
 //v({process.env.npm_package_version}
 import React, { useState } from "react";
-import { Grommet, Box, Heading, Paragraph, TextInput, Button } from "grommet";
+import {
+  Grommet,
+  Box,
+  Heading,
+  Paragraph,
+  TextInput,
+  Button,
+  Text
+} from "grommet";
 
 //theme
 const theme = {
@@ -36,45 +44,79 @@ const listBoxProps = {
   }
 };
 
-const headingProps = {
-  level: 1
-};
-
-const paragraphProps = {
-  textAlign: "center"
-};
-
-const textInputProps = {
-  placeholder: "Add a thing...",
-  size: "medium"
-};
-
 export function App() {
   const [inputValue, setInputValue] = useState("");
+  const [thingsArray, setThingsArray] = useState([]);
 
-  const onChange = event => setInputValue(event.target.value);
+  const onChange = event => {
+    setInputValue(event.target.value);
+  };
+
+  const onKeyDown = event => {
+    if (event.which === 13) {
+      addThing();
+    }
+  };
+
+  const addThing = () => {
+    if (inputValue.trim() !== "") {
+      setThingsArray([...thingsArray, inputValue]);
+      setInputValue("");
+    }
+  };
+
+  const lessThan3Things = thingsArray.length < 3;
 
   return (
     <Grommet theme={theme}>
       <Box {...boxProps}>
-        <Heading {...headingProps}> All The Things</Heading>
-        <Paragraph {...paragraphProps}>
+        <Heading level={1}> All The Things</Heading>
+        <Paragraph textAlign="center">
           Let's prioritize! Start by adding a handful of things below.
         </Paragraph>
         <Box {...textInputBoxProps}>
           <TextInput
-            {...textInputProps}
+            placeholder="Add a thing..."
+            size="medium"
             value={inputValue}
             onChange={onChange}
+            onKeyDown={onKeyDown}
           />
-          <Button label="+Add" onClick={() => {}} margin="small" primary />
+          <Button label="+Add" onClick={addThing} margin="small" />
         </Box>
         <Box {...listBoxProps}>
           <Paragraph color="dark-5">
-            Add at least 3 things to start prioritizing
+            {lessThan3Things &&
+              `Add ${3 - thingsArray.length} more things to compare`}
           </Paragraph>
+          {thingsArray.map((thing, index) => {
+            return <Thing key={index} text={thing} />;
+          })}
+          {!lessThan3Things && (
+            <Button
+              primary
+              label="Compare"
+              margin="small"
+              onClick={() => {
+                console.log("Compare Button Clicked");
+              }}
+            />
+          )}
         </Box>
       </Box>
     </Grommet>
+  );
+}
+
+function Thing(props) {
+  return (
+    <Box
+      border={{ color: "dark-1", size: "small" }}
+      margin="xsmall"
+      pad="small"
+      width="medium"
+    >
+      <Text>{props.text}</Text>
+    </Box>
   );
 }
