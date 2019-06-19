c84c1eaf9b6928e8826c26ab901bb38e7ee40f84
Omar Hyjek <ohyjek@gmail.com>
2019-06-19 12:54:01 -0400

    first attempt at landing page (no functions)

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 //v({process.env.npm_package_version}
-import React from "react";
-import { Grommet, Box, Heading, Paragraph } from "grommet";
+import React, { useState } from "react";
+import { Grommet, Box, Heading, Paragraph, TextInput, Button } from "grommet";
 
 //theme
 const theme = {
@@ -16,11 +16,27 @@ const theme = {
 const boxProps = {
   pad: "small",
   a11yTitle: "Main Container",
-  align: "center",
-  alignContent: "start"
+  align: "center"
 };
 
-const h1Props = {
+const textInputBoxProps = {
+  align: "center",
+  width: "medium",
+  direction: "row"
+};
+
+const listBoxProps = {
+  margin: "medium",
+  align: "center",
+  width: "medium",
+  border: {
+    color: "brand",
+    size: "small",
+    side: "top"
+  }
+};
+
+const headingProps = {
   level: 1
 };
 
@@ -28,14 +44,36 @@ const paragraphProps = {
   textAlign: "center"
 };
 
+const textInputProps = {
+  placeholder: "Add a thing...",
+  size: "medium"
+};
+
 export function App() {
+  const [inputValue, setInputValue] = useState("");
+
+  const onChange = event => setInputValue(event.target.value);
+
   return (
     <Grommet theme={theme}>
       <Box {...boxProps}>
-        <Heading {...h1Props}> All The Things</Heading>
+        <Heading {...headingProps}> All The Things</Heading>
         <Paragraph {...paragraphProps}>
           Let's prioritize! Start by adding a handful of things below.
         </Paragraph>
+        <Box {...textInputBoxProps}>
+          <TextInput
+            {...textInputProps}
+            value={inputValue}
+            onChange={onChange}
+          />
+          <Button label="+Add" onClick={() => {}} margin="small" primary />
+        </Box>
+        <Box {...listBoxProps}>
+          <Paragraph color="dark-5">
+            Add at least 3 things to start prioritizing
+          </Paragraph>
+        </Box>
       </Box>
     </Grommet>
   );
