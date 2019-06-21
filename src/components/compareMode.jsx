a5f2001b1629dca947bfaae9dc2cb22ca4d8c4f0
Omar Hyjek <ohyjek@gmail.com>
2019-06-21 15:06:41 -0400

    update comare screen

--- src/components/compareMode.jsx
+++ src/components/compareMode.jsx
@@ -1,17 +1,57 @@
 import React from "react";
-import { Heading, Button } from "grommet";
+import { Heading, Button, Paragraph, Box } from "grommet";
+
+const compareHeadingBoxProps = {
+  margin: {
+    top: "medium"
+  },
+  align: "center",
+  width: "medium",
+  border: {
+    color: "brand",
+    size: "small",
+    side: "top"
+  }
+};
+
+const compareBoxProps = {
+  margin: "medium",
+  align: "center",
+  width: "large",
+  height: "small",
+  direction: "row"
+};
+
+const thingBoxProps = {
+  margin: "medium",
+  align: "center",
+  justify: "center",
+  width: "medium",
+  fill: "vertical",
+  alignContent: "center"
+};
 
 export function CompareMode(props) {
   const { onModeChangeClick } = props;
 
   return (
     <>
-      <Heading level="3">{"Compare Mode!"}</Heading>
-      <Button
-        primary
-        label="Go back to Input Mode"
-        onClick={onModeChangeClick}
-      />
+      <Paragraph textAlign="center">{"Let the prioritizing begin!"}</Paragraph>
+      <Box {...compareHeadingBoxProps}>
+        <Heading level="3">{"Which thing is more important?"}</Heading>
+      </Box>
+      <Box {...compareBoxProps}>
+        <Box {...thingBoxProps}>
+          <Button fill label="Example Text" />
+        </Box>
+        <Box {...thingBoxProps}>
+          <Button fill label="Example Text" />
+        </Box>
+      </Box>
+      <Box pad="small" direction="row" gap="medium">
+        <Button label="Oops" onClick={onModeChangeClick} />
+        <Button primary label="Next" onClick={() => {}} />
+      </Box>
     </>
   );
 }
